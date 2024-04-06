c381d3eb7ddc559d331112001affd1c7700f1502
Sylvia Joy Bintrim <sjb2225@ruth.cm.cluster>
2024-04-06 18:05:55 -0400

    cleaned up crpa supercell

--- cRPA/crpa_supercell.py
+++ cRPA/crpa_supercell.py
@@ -104,123 +104,129 @@ if __name__ == '__main__':
     from pyscf.pbc import gto, dft
     from crpa import cRPA_CASCI
     from ase.lattice.hexagonal import Graphene 
-    
-    import multiprocessing
-    from multiprocessing import Pool
-    print('number of logical cores', multiprocessing.cpu_count())
-    
-    def task(i):
-        with open('madelung_screened_dzv_{}.txt'.format(i), 'w') as scr:
-            with open('madelung_unscreened_dzv_{}.txt'.format(i), 'w') as uscr:
-                nx = i
-                ny = i
-               
-                cell = gto.Cell()
-                cell.unit = 'b'
-                atoms = open("atoms_{}.txt".format(i), "r")
-                cell_abc = open("cell_{}.txt".format(i), "r")
-                cell.atom = atoms.read()
-                cell.a = cell_abc.read()
-                
-                cell.basis = 'gth-dzv'
-                cell.pseudo = 'gth-pade'
-                cell.ke_cutoff = 80.
-                cell.verbose = 5
-                cell.build()
-                
-                mf = dft.RKS(cell).density_fit()
-                mf.xc = 'pbe'
-#                mf.with_df._cderi_to_save = 'hbn_c2_dzv_{}.h5'.format(nx)
-                mf.with_df._cderi = 'hbn_c2_dzv_{}.h5'.format(nx)
-                # mf.chkfile = 'hbn_c2_dzv_{}.chk'.format(nx)
-                dm = mf.from_chk('hbn_c2_dzv_{}.chk'.format(nx))
-#                mf.chkfile = 'hbn_c2_dzv_{}.chk'.format(nx)
-                 
-                mf.kernel(dm)
-#                mf.kernel()
-                
-                # from pyscf.pbc.scf.chkfile import load_scf
-                # cell, scf_res = load_scf('hbn_c2_gdf_{}x{}x1.chk'.format(nx,ny))
-                # cell.verbose = 6
-                # mf = dft.RKS(cell).density_fit()
-                # mf.with_df._cderi = 'hbn_c2_pbc_gdf_{}x{}x1.h5'.format(nx,ny)
-                # mf.xc = 'pbe'
-                # mf.mo_coeff = scf_res['mo_coeff']
-                # mf.mo_energy = scf_res['mo_energy']
-                # mf.mo_occ = scf_res['mo_occ']
-                # mf.e_tot = scf_res['e_tot']
-                # mf.converged = True
-                # energy_nuc = mf.energy_nuc()
-                
-                nocc = cell.nelectron//2
-                nmo = mf.mo_energy.size
-                nvir = nmo - nocc
-                print('HOMO E: ', mf.mo_energy[nocc-1], 'LUMO E: ', mf.mo_energy[nocc])
-                
-                # Using P-M to mix and localize the HOMO/LUMO
-#                from pyscf import lo
-#                idcs = np.ix_(np.arange(nmo), [nocc-1, nocc])
-#                mo_init = lo.PM(cell, mf.mo_coeff[idcs])
-#                C_loc = mo_init.kernel()
-#                lib.chkfile.dump('hbn_c2_dzv_{}.chk'.format(nx), 'C_loc', C_loc)
-                
-                C_loc = lib.chkfile.load('hbn_c2_dzv_{}.chk'.format(nx), 'C_loc')
-                
-                mycRPA = cRPA_supercell(mf, 'hbn_c2_dzv_{}.h5'.format(nx), loc_coeff = C_loc)
-                my_unscreened_eris = mycRPA.kernel(screened = False)
-                my_screened_eris = mycRPA.kernel(screened = True)
-                # print('hBN+C2 C2pz unscreened ERIs (eV)', my_unscreened_eris*27.2114)
-                # print('hBN+C2 C2pz screened ERIs (eV)', my_screened_eris*27.2114)
-                
-                from pyscf import mcscf, fci
-             
-                uscr.write('cRPA_CASCI, unscreened U \n')
-                ncas  = 2
-                ne_act = 2
-                mycas = cRPA_CASCI(mf, ncas, ne_act, screened_ERIs = my_unscreened_eris)
-                mycas.canonicalization = False
-                mycas.verbose = 6
-                orbs = np.hstack( ( np.hstack( (mf.mo_coeff[:, :nocc-1], C_loc) ), mf.mo_coeff[:, nocc+1:] ) )
-                mycas.fcisolver.nroots = 4
-                mycas.kernel(orbs)
-                uscr.write('t={} \n'.format(mycas.get_h1eff()))
-                uscr.write('n={} Exc={} U={}'.format(nx, mycas.e_tot, (my_unscreened_eris[0,0,0,0]+my_unscreened_eris[1,1,1,1])/2))
-                uscr.write('\n')
-                uscr.write('unscreened eris \n')
-                for i in range(2):
-                    for j in range(2):
-                        for k in range(2):
-                            for l in range(2):
-                                uscr.write('({}{}|{}{})={} \n'.format(i,j,k,l, my_unscreened_eris[i,j,k,l]*27.2114))
-                for exc in mycas.e_tot:
-                    uscr.write('{}'.format(27.2114*(exc-mycas.e_tot[0])))
-                    uscr.write('\n')
-                
-                scr.write('cRPA_CASCI, screened U \n')
-                mycas = cRPA_CASCI(mf, ncas, ne_act, screened_ERIs = my_screened_eris)
-                mycas.canonicalization = False
-                mycas.verbose = 6
-                # orbs = np.hstack( ( np.hstack( (mf.mo_coeff[:, :nocc-1], C_loc) ), mf.mo_coeff[:, nocc+1:] ) )
-                mycas.fcisolver.nroots = 4
-                mycas.kernel(orbs)
-                scr.write('t={} \n'.format(mycas.get_h1eff()))
-                scr.write('n={} Exc={} U={}'.format(nx, mycas.e_tot, (my_screened_eris[0,0,0,0]+my_screened_eris[1,1,1,1])/2))
-                scr.write('\n')
-                scr.write('screened eris \n')
-                for i in range(2):
-                    for j in range(2):
-                        for k in range(2):
-                            for l in range(2):
-                                scr.write('({}{}|{}{})={} \n'.format(i,j,k,l, my_screened_eris[i,j,k,l]*27.2114))
-                for exc in mycas.e_tot[1:]:
-                    scr.write('{}'.format(27.2114*(exc-mycas.e_tot[0])))
-                    scr.write('\n')
+    import os 
+    import sys
+    atoms_cell_path = '/burg/berkelbach/users/sjb2225/v2.4.0/crpa/github/crpa/hbn_c2_geometries/'
+    from pyscf.lib import chkfile
+
+    i =  sys.argv[1]
+    basis = 'dzvp'
+    fnl = 'PBE'
+    with open('screened_{}_{}.txt'.format(basis, i), 'w') as scr:
+        with open('unscreened_{}_{}.txt'.format(basis, i), 'w') as uscr:
+            nx = i
+            ny = i
+            cell = gto.Cell()
+            cell.unit = 'b'
+            atoms = open(atoms_cell_path+"atoms_{}.txt".format(i), "r")
+            cell_abc = open(atoms_cell_path+"cell_{}.txt".format(i), "r")
+            cell.atom = atoms.read()
+            cell.a = cell_abc.read()
             
-        return None    
-    
-    # create the process pool
-    with Pool() as pool:
-        # call the same function with different data in parallel
-        for result in pool.map(task, range(2,3)):
-            # report the value to show progress
-            print(result)
+            cell.basis = 'gth-'+basis
+            cell.pseudo = 'gth-'+fnl
+            cell.ke_cutoff = 80.
+            cell.verbose = 5
+            cell.build()
+                        
+            gdf = df.GDF(cell)
+            gdf_fname = '{}_{}_{}.h5'.format(fnl, basis, i)
+            gdf._cderi_to_save = gdf_fname
+            if not os.path.isfile(gdf_fname):
+                gdf.build()
+
+            chkfname = '{}_{}_{}.chk'.format(fnl, basis, i)
+            if os.path.isfile(chkfname):
+                mf = dft.RKS(cell)
+                mf.xc = fnl
+                mf.with_df = gdf
+                mf.with_df._cderi = gdf_fname
+                data = chkfile.load(chkfname, 'scf')
+                mf.__dict__.update(data)
+            else:
+                mf = dft.RKS(cell)
+                mf.xc = fnl
+                mf.with_df = gdf
+                mf.with_df._cderi = gdf_fname
+                mf.conv_tol = 1e-12
+                mf.chkfile = chkfname
+                mf.verbose = 9
+                mf.kernel()
+            
+            nocc = cell.nelectron//2
+            print('nocc', nocc)
+            nmo = mf.mo_energy.size
+            nvir = nmo - nocc
+            print('HOMO E: ', mf.mo_energy[nocc-1], 'LUMO E: ', mf.mo_energy[nocc])
+            
+            
+            C_loc = lib.chkfile.load(chkfname, 'C_loc')
+            if C_loc is None:
+                # Using P-M to mix and localize the HOMO/LUMO
+                from pyscf import lo
+                idcs = np.ix_(np.arange(nmo), [nocc-1, nocc])
+
+                from pyscf.tools import mo_mapping
+                comp = mo_mapping.mo_comps('C 2pz', cell, mf.mo_coeff)
+                C2pz_idcs = np.argsort(-comp)[:5]
+                for idx in C2pz_idcs:
+                    print(f'AO {idx} has {comp[idx]} C2pz character')
+
+                mo_init = lo.PM(cell, mf.mo_coeff[idcs])
+                C_loc = mo_init.kernel()
+                lib.chkfile.dump(chkfname, 'C_loc', C_loc)
+                
+                from pyscf.tools import cubegen
+                for orbnum in range(2):
+                    cubegen.orbital(cell, f'canon_{basis}_{i}_mo{orbnum+1}.cube', mf.mo_coeff[idcs][:,orbnum])
+                    cubegen.orbital(cell, f'loc_{basis}_{i}_mo{orbnum+1}.cube', C_loc[:,orbnum])
+            
+            mycRPA = cRPA_supercell(mf, gdf_fname, loc_coeff = C_loc)
+            my_unscreened_eris = mycRPA.kernel(screened = False)
+            my_screened_eris = mycRPA.kernel(screened = True)
+            #print('hBN+C2 C2pz unscreened ERIs (eV)', my_unscreened_eris*27.2114)
+            #print('hBN+C2 C2pz screened ERIs (eV)', my_screened_eris*27.2114)
+            
+            from pyscf import mcscf, fci
+         
+            uscr.write('cRPA_CASCI, unscreened U \n')
+            ncas  = 2
+            ne_act = 2
+            mycas = cRPA_CASCI(mf, ncas, ne_act, screened_ERIs = my_unscreened_eris)
+            mycas.canonicalization = False
+            mycas.verbose = 6
+            orbs = np.hstack( ( np.hstack( (mf.mo_coeff[:, :nocc-1], C_loc) ), mf.mo_coeff[:, nocc+1:] ) )
+            mycas.fcisolver.nroots = 5
+            mycas.kernel(orbs)
+            uscr.write('t={} \n'.format(mycas.get_h1eff()))
+            uscr.write('n={} Exc={} U={}'.format(nx, mycas.e_tot, (my_unscreened_eris[0,0,0,0]+my_unscreened_eris[1,1,1,1])/2))
+            uscr.write('\n')
+            uscr.write('unscreened eris \n')
+            for i in range(2):
+                for j in range(2):
+                    for k in range(2):
+                        for l in range(2):
+                            uscr.write('({}{}|{}{})={} \n'.format(i,j,k,l, my_unscreened_eris[i,j,k,l]*27.2114))
+            for exc in mycas.e_tot:
+                uscr.write('{}'.format(27.2114*(exc-mycas.e_tot[0])))
+                uscr.write('\n')
+            
+            scr.write('cRPA_CASCI, screened U \n')
+            mycas = cRPA_CASCI(mf, ncas, ne_act, screened_ERIs = my_screened_eris)
+            mycas.canonicalization = False
+            mycas.verbose = 6
+            # orbs = np.hstack( ( np.hstack( (mf.mo_coeff[:, :nocc-1], C_loc) ), mf.mo_coeff[:, nocc+1:] ) )
+            mycas.fcisolver.nroots = 4
+            mycas.kernel(orbs)
+            scr.write('t={} \n'.format(mycas.get_h1eff()))
+            scr.write('n={} Exc={} U={}'.format(nx, mycas.e_tot, (my_screened_eris[0,0,0,0]+my_screened_eris[1,1,1,1])/2))
+            scr.write('\n')
+            scr.write('screened eris \n')
+            for i in range(2):
+                for j in range(2):
+                    for k in range(2):
+                        for l in range(2):
+                            scr.write('({}{}|{}{})={} \n'.format(i,j,k,l, my_screened_eris[i,j,k,l]*27.2114))
+            for exc in mycas.e_tot[1:]:
+                scr.write('{}'.format(27.2114*(exc-mycas.e_tot[0])))
+                scr.write('\n')
---
+++ cRPA/run_crpa_supercell.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=berkelbach
+#SBATCH --job-name hbn_c2_7_dzvp
+#SBATCH --time=48:30:00
+#SBATCH --cpus-per-task=32
+#SBATCH --ntasks=1
+#SBATCH --mem=700000MB 
+#SBATCH --array=7
+
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-type=fail
+#SBATCH --mail-user=sjb2225@columbia.edu
+
+JOBNAME=$SLURM_JOB_NAME
+
+. /burg/berkelbach/users/sjb2225/build/spack/share/spack/setup-env.sh
+spack env activate -p pyscf
+export PYTHONPATH=/burg/berkelbach/users/sjb2225/build/pyscf:$PYTHONPATH
+export PYSCF_MAX_MEMORY=690000
+export OMP_NUM_THREADS=32
+
+arr=(${SLURM_ARRAY_TASK_ID})
+echo "$(python crpa_supercell.py ${arr[@]})" >> "hbn_c2_dzvp_"${arr[0]}".txt"
