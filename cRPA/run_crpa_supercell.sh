6d3794733960f8f3c5997b7f14c5f2f71cce8a57
Sylvia Bintrim <51516247+SylviaJoy1@users.noreply.github.com>
2024-09-11 11:03:41 -0400

    Add files via upload

--- cRPA/run_crpa_supercell.sh
+++ cRPA/run_crpa_supercell.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=berkelbach
-#SBATCH --job-name hbn_c2_6_7_dzvp
-#SBATCH --time=48:30:00
+#SBATCH --job-name hbn_c2_2_dzvp
+#SBATCH --time=1:30:00
 #SBATCH --cpus-per-task=32
 #SBATCH --ntasks=1
-#SBATCH --mem=700000MB 
-#SBATCH --array=6-7
+#SBATCH --mem=64000MB 
+#SBATCH --array=2
 
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
@@ -17,7 +17,7 @@ JOBNAME=$SLURM_JOB_NAME
 . /burg/berkelbach/users/sjb2225/build/spack/share/spack/setup-env.sh
 spack env activate -p pyscf
 export PYTHONPATH=/burg/berkelbach/users/sjb2225/build/pyscf:$PYTHONPATH
-export PYSCF_MAX_MEMORY=690000
+export PYSCF_MAX_MEMORY=64000
 export OMP_NUM_THREADS=32
 
 python crpa_supercell.py ${SLURM_ARRAY_TASK_ID} > hbn_c2_dzvp_${SLURM_ARRAY_TASK_ID}.txt
